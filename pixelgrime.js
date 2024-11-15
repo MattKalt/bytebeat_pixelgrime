4cbb3430db8bbf23f2215d168c2ae8dba529ed45
Matt Kalt <93488504+MattKalt@users.noreply.github.com>
2024-11-14 21:42:30 -0500

    revamped reverb

--- pixelgrime.js
+++ pixelgrime.js
@@ -3,7 +3,7 @@
 
 	H 4 X X 3 D    U P    F O R    B A R B E C U E
 
-	using the GAv2 tech and a revamped stereo reverb function partly inspired by Feeshbread's Dead Data
+	using the GAv2 tech and a revamped stereo reverb/chorus function
 
 	T U R N    Y O U R    V O L U M E    U P !
 
@@ -14,22 +14,43 @@ T = t,
 
 t *= r8 = 10 / 48,
 
-t-=t/8&128,		//subtle swang
-//t-=t/4&256,		//heavy swang
-//t-=t/8&128*7,	//5/4 time
-//t-=t/4&128*7,	//3/4 time
+
+//seq = ( arr, spd, t2=t ) => arr[ (t2 >> spd) % arr.length ],
+/*version that lerps:
+	the 'x' argument controls the speed at which the slides happen (1=very slidy, 99=almost none, 0=none) */
+seq=(r,s,t2=t,x=0)=>(i=t2/2**s,J=i|0,L=r.length,x?(k=(i-J)**x,(1-k)*r[J%L]+k*r[(J+1)%L]):r[J%L]),
+
+//----SONG SETTINGS-----
+
+//master pitch
+mp = seq([0,-2,1,-3],18) - 3.5,
 
 
-
-//master pitch, CHANGES BELOW
-mp = -3.5,
-//mp = -5, //for r8=11
+//t>>9>3800&&(t=1,mp=-8), //the "Bluescreen"
+T>93e5&&(t=1,mp=-8), //the "Bluescreen"
 
 
-// Repeat x beats of y
-// SUPER useful if you're writing complex beats/melodies
-// Include this or the Fs won't work (or you could replace r(x, y) with Array(x).fill(y))
-// r(1,[arrays]) also serves as a replacement for [arrays].flat()
+//Changing rhythms:
+t-=seq([
+	t/4&896,
+	0,
+	t/8&128,
+	t/2&8, //funny tom
+	t/8&128,
+	t/4&896,
+	t/8&128,
+	t-1
+],18,t,seq('1120004',18)), //glich 3
+
+
+//-----TOOLS-----
+
+/*
+	Repeat x beats of y
+	SUPER useful if you're writing complex beats/melodies
+	Include this or the Fs won't work (or you could replace r(x, y) with Array(x).fill(y))
+	r(1,[arrays]) also serves as a replacement for [arrays].flat()
+*/
 r = repeat = (x, y) => Array( x ).fill( y ).flat( 9 ),
 
 sp = (str, sep='') => str.split( sep ),
@@ -38,37 +59,21 @@ j = (arr, sep='') => arr.join( sep ),
 //tra = transpose = (arr, amt) => arr.map(x=>x+amt),
 tra = transpose = (x, amt) => Array.isArray(x)? x.map( e => e + amt ) : j( sp(x).map( e => e + amt ) ),
 
-
-
-// Uses up a lot of chars and isn't /super/ readable, but a major timesaver when creating
-// Particularly the NaN handing
+//pretty much deprecated but used in bt()
 m = mix = (x, vol=1, dist=0) => ( ( x * vol * ( 1 + dist ) ) % ( 256 * vol ) ) || 0,
 
-// F is the FX stack, stores memory for use in effects
-// Automatically keeps track of what's stored where
-// If you see red (NaNs), raise 26e3 higher, or adjust your reverbs' 'dsp' variable (and limiters' lookahead)
-// Works best when effects are not inside conditionals (meaning the number of F in use changes)
-// But even then, should only create a momentary click/pop (might be more severe for reverb)
-T ? 0 : F = r( 4096, 0 ),
-// Iterator, resets to 0 at every t
+/*
+	F is the FX stack, stores memory for use in effects
+	Automatically keeps track of what's stored where
+	If you see red (NaNs), raise 2032 higher, or adjust your reverbs' 'dsp' variable (and limiters' lookahead)
+	Works best when effects are not inside conditionals (meaning the number of F in use changes)
+	But even then, should only create a momentary click/pop (might be more severe for reverb)
+*/
+T ? 0 : F = r( 2048, 0 ),
+// Index of F, resets to 0 at every t
 I = 0,
 
-
-//seq = ( arr, spd, t2=t ) => arr[ (t2 >> spd) % arr.length ],
-/*version that lerps:
-	the 'x' argument controls the speed at which the slides happen (1=very slidy, 99=almost none, 0=none) */
-//seq=(r,s,t2=t,x=0)=>(i=t2/2**s,J=i|0,k=(i-J)**x,L=r.length,x?(1-k)*r[J%L]+k*r[(J+1)%L]:r[J%L]),
-/*v slight perf boost:*/
-seq=(r,s,t2=t,x=0)=>(i=t2/2**s,J=i|0,L=r.length,x?(k=(i-J)**x,(1-k)*r[J%L]+k*r[(J+1)%L]):r[J%L]),
-
-mp = seq([0,-2,1,-3],18) - 3.5, //this is where the master pitch changes
-
-//t -= seq([t/4&256,0,t/8&128,t/8&128*7,t/4&128*7],9,t>>9,4),
-
-//t -= seq([t/4&896,0,t/4&896,t/8&128,t/8&128,t/4&896,t/8&128,t/4,t/2,t-1],18,t,seq('19100008',18)), //ultraglitch
-//t-= seq([t/4&896,0,0,t/4&896,t/8&128,t/4&896,t/8&128,t/4,t/2,t-1],18,t,seq('10000008',18)), //broken :(
-
-
+//melodic sequences without clicks/pops
 //mseq = ( ...x ) => t * 2 ** ( seq(...x) / 12 ), //original
 mseq = ( ...x ) => (
 	F[I++] += ( r8 * 2 ** ( ( seq(...x) + mp ) / 12))||0
@@ -97,40 +102,60 @@ h = 1 & T * 441/480, // long Hihat
 h = seq( [h,h,h,0], 8), //quieter, faster attack
 
 /*
+	Stereo delay with multiple heads (sorta similar to feeshbread's multitap delay in Dead Data)
 	single input, outputs an array size 2
 	t2 and vibratospeed must have the same length (arbitrary)
-	requires old lp(), new hp(), and slidy seq() to function
+	requires old lp(), new hp(), lim2(), r(), and slidy seq() to function
 */
 
-rvs = reverbStereo = ( input, len = 16e3, vibratoSpeed = [1,2], wet = .7, dry = .4, feedb = 1, dsp = 3, lerpx=4, highpass=.03, compSpeed = .1, compThresh = 64, t2 = [T,T], vibratoDepth = 99 ) => (
-	o=[],peak=[],//out=[0,0] //can reuse o
+rvs = reverbStereo = ( input, len = 16e3, vibratoSpeed = [7,9], dry = .4, wet = 1, dsp = 3, lerpx=4, highpass=.03, lowpass = .5, compAtk = 9, compRel = 9, compThresh = 64, vibratoDepth = 99, t2 ) => (
+	vcs = vibratoSpeed.length,
+	t2 ??= r(vcs, T ), //array of all T the same size as vibratoSpeed[], could also be T/2 if specified in args
+	o=[],//out=[0,0] //can reuse o
 	t2.map( (t2val,i) => (
-		t2val += vibratoDepth * 2 +  vibratoDepth * sin(T*vibratoSpeed[i]/3e5),
-		o[i] = hp( input*dry + feedb * seq( F, 0, I + t2.length*2 + ( t2val % len ) / dsp, lerpx ) || 0, highpass),
-		peak[i] = lp( max( compThresh, abs( o[i] ) ), compSpeed, 99 ),
-		o[i] *= wet * compThresh / max( peak[i], compThresh )
+		t2val += vibratoDepth*2 +  vibratoDepth * sin(T*vibratoSpeed[i]/3e6),
+		o[i] = hp(
+			input * dry/2 + wet*300/compThresh/vcs * seq(
+				F, 0, I + vcs*2-i + ( t2val % len ) / dsp, lerpx //more accurate but removing isn't a problem
+				//F, 0, I + ( t2val % len ) / dsp, lerpx
+			) || 0
+			, highpass
+		)
+		//,o[i] = lim2(o[i],compAtk,compRel,compThresh) //lim()ing here creates more feedback
 	)),
-	F[ I + ( (T % len) / dsp )|0 ] = o.reduce((a,e)=>a+=e),
+	F[ I + vcs + 1 + ( (T % len) / dsp )|0 ] = lp2(
+		o.reduce((a,e,i)=> a=lim2(
+			//a+e, compAtk,compRel,compThresh/vcs*(1+i/2)
+			a+e, compAtk,compRel,compThresh/vcs*(1+i/2)
+		)
+	), lowpass ),
 	I += 0|(len / dsp),
 	o.map((e,i)=>o[i%2]+=e),o //first 2 voices will be double volume
 	//o.map((e,i)=>out[i%2]+=e),out
 ),
 
-
-lp = lopass = (x, f, bias=1) => ( // f ~= frequency, but not 1:1
+//bad lopass (turns things into triangles rather than sins) but good for compressor
+lp = lopass = (input, freq, bias=1) => ( // f ~= frequency, but not 1:1
 	// F[I] is the value of the last sample
 	// You will need to change the 'x % 256' if you're using signed or floatbeat
-	F[I] = min( max( x % 256, F[I] - f), F[I++] + f * bias) // Clamp the change since last sample between (-f, f)
+	F[I] = min( max( input, F[I] - freq), F[I++] + freq * bias) // Clamp the change since last sample between (-f, f)
 ),
 
-
-
-lp2 = (x,f) =>
-	F[I] = F[I++] * (1-f) + x * f
+//better lopass, especially for hi-pass
+lp2 = (input,freq) =>
+	F[I] = F[I++] * (1-freq) + input * freq
 ,
 
-hp = (x,f) => x - lp2(x,f),
+hp = (input,freq) => input - lp2(input,freq),
 
+//simple but bad limiter, uses the bad lopass
+//release must be >0
+lim2 = (input, atk, release, thresh) => (
+	input * thresh / lp(
+		max( thresh, abs(input))
+	,release, atk/release
+	)
+),
 
 /*
 The limiter is probably the most advanced effect here
@@ -183,18 +208,19 @@ hm = harmonify = (x,tone, waveTableSize = 8) => (
 ),
 
 
+/*
+	Version 1 of my Synth
+A new version has been developed, but this is the one from GAv2 that I have a lot of tones for
 
-// Version 1 of my Synth
-// A new version has been developed, but I lost it lmao
-
-//Basically just treat this like a black box and fiddle with the knobs at random
-//For a more detailed exmplanation:
-//	X, and the First 2 hexes of y, are the fun surprise knobs :)
-//		Small changes in these values completely change the tone (most of the time)
-//	The next 2 hexes of y control the harmonifier
-// The next hex controls the *thump*/click/noise of the attack
-// The next hex controls the decay
-// The next 2 hexes control the lowpass
+Basically just treat this like a black box and fiddle with the knobs at random
+For a more detailed exmplanation:
+	X, and the First 2 hexes of y, are the fun surprise knobs :)
+		Small changes in these values completely change the tone (most of the time)
+	The next 2 hexes of y control the harmonifier
+	The next hex controls the thump/click/noise of the attack
+	The next hex controls the decay
+	The next 2 hexes control the lowpass
+*/
 sy = synth = (melody, velTrack, speed, x, y, ...z)=>
 	lp(
 		min(
@@ -306,7 +332,16 @@ l1a = [5,7,8,10,1,4,-2,3],
 tn1 = [0x71010559, 0x610105ff],
 ta = r(1, [ r(4, 0x712b2321), r(2, 0xa21b02a6 ), 0x63010201 ] ),
 
-vv=[11,12,13,14,15],
+//vs=[11,12,13,14,15],
+//vs=[2,3,5,7,11,13],
+//vs=[3,5,7,11,13,17,19,23,29,31],
+//vs=[10,30,50,70,110,130,170,190,230,290,310],
+//vs=[41,42,43,44,45,46,47,48,49,50],
+vs=r(16,1).map((e,i)=>e*1.618**(i/2)),
+//vv=[2,2,2,2,2],
+
+//vv=[PI,4],
+
 
 bta = "1 h s h 1 h s1hh",
 btb = "1 h s h11 h1s1h1",
@@ -334,60 +369,63 @@ L1 = ch => sy( mseq(m5,10,t,0), v5, 11, 1.07, tn1[ch] ) * (1+ch/8),
 L2 = [L1(0),L1(1)], //cacheing for performance
 
 L3 = mseq(l1a,15,t,2)^mseq(l1a,15,t,8),
-
 L3 = L3*2&t>>5&31,
+L3 *= seq(l3v,17,t,1)/5,
 
-//K = (sin(cbrt(199*(t%1024)))*127+(t/2&127))*bt(drk,10,1)**(1/8),
 K = (sin(sqrt(6*(t%1024)))*127+(t/2&127))*bt(drk,10,1)**(1/8),
 HH = bt([h],10)*seq(drh,10),
 SN = bt([s],10)*seq(drs,10),
+//T*r8>>18>5?K=SN=0:0, //drums silent during the glitch part
+T*r8>>18==6?K=SN=0:0, //drums silent during the rewind
+//T*r8>>18==6?K/=9:0, //drums quieter during the rewind
+
 
 DR = HH + SN + K * 3,
 
-//L3 = s2s(mseq(l1a,15,t,4)*8)/8,
-
 A1 = sy( mseq(a2,10),[1],10, 4.6, seq(ta,9,t>>9,1)),
 //A1 = sy( mseq(a2,10),[1],10, 4.6, 0xa21b02a6),
+A1 *= seq(av,18),
 
 B1 = mseq(b2,10,t,0) & 255 * seq(vb2,11),
 B2 = s2s(B1),
 B3 = (-B1 & B2),
 
-//V = rvs( A1 + L2[0], 20e3, [11,12,13,14,15], 2-(t/512%2), .2, 1, (t>>16)+1, 4, .1, .1, 16, [T,T+11e4,T+13e4,T+17e4,T+19e4], 99 ),
 
-A1 *= seq(av,18),
-L3 *= seq(l3v,17,t,1)/9,
 
 vl = 2-(t/512%2),
-fb=[vl/2+1,vl+.3,vl/2+1,vl],
-
-//Mute
-t>>9>=3074&&(L2=fb=[0.5,0],A1=L3=DR=B3=0),
-//t>>9>=3586&&(L2=fb=[0.5,0],A1=L3=DR=B3=0),
-//t>>9>=4098&&(L2=fb=[0.5,0],A1=L3=DR=B3=0),
+//vv = seq( [vl/2+1, vl+.5, vl/2+1, vl+.2], 18),
+vv = seq( [vl*4+9, vl*8+4, vl*4+9, vl*8+2], 18),
 
 
+//t<9?vv=69:0, //held note during "bluescreen" has more reverb
 
-V = rvs( vl * (A1/3 + L2[0]/2) + 2 * L3, 9e3, vv, seq(fb,18), .4, 1, 4, 4, .1, .1, 16, [T,T,T,T,T], 99 ),
 
-
+V = rvs( vl * (A1/3 + L2[0]/2) + L3, 8e3, vs, .1, .9, 4, 0, .1, .5, 9, .1, 32, 699 ),
 
 //Master = ch => lim(
 Master = ch => tanh(hp(
 
-( L2[ch]*.6 + B3 + A1/9 + DR ) * min(1,t/1e6+.5)  +
+//		dry
+( L2[ch]*.6 + B3 + A1/9 + DR ) * 1 +
+//		reverb
+//lp2( V[ch], min(1,t/2e5+.1)) * min(4, T/5e4, 2+t/5e5)
 
-//rv(L2[0]*2, 24e3, 2.1,.2,.5,4,T,.03,2,99,ch+.5)/3
+lp2( V[ch], min(1,t/2e5+.1)) * vv/6
 
-lp2( V[ch], min(1,t/2e5+.1)) * min(1.3,t/6e5+.3)
+
+/*
+//		dry
+( L2[ch]*.6 + B3 + A1/9 + DR ) * (min(1,t/1e6+.5)==1?1:0)  + //50s
+//		reverb
+lp2( V[ch], min(1,t/2e5+.1)) * (min(3.9,t/2e5+.9)==3.9?3.9:0) //64s
+*/
 
 //, 1, 512, 1, 150 ),
-,.001)/max(150,300-t/8e3))*1.1,
+,.001)/max(170,300-t/5e3,600-t/999))*1.1,
+
+
 
 [Master(0), Master(1)]
-
-
-
 
 
 //,a=()=>{throw(I)},a() //Determine size of memory stack to initialize
